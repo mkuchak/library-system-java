464f103420f0f71fbfb5f2930c155b7d4eec16b7
williampats <40124068+williampats@users.noreply.github.com>
2018-06-09 15:33:49 -0300

    .
    .

--- src/br/com/acme/AcademicLibrary.java
+++ src/br/com/acme/AcademicLibrary.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
  */
 public class AcademicLibrary extends ILibray {
 
-    private String testeGit;
     private String name;
     private String description;
     private String creationDate;
