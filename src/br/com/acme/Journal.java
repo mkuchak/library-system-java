1a61668e4ced75d7cb747ae49db149b9cd35c1d1
williampats <40124068+williampats@users.noreply.github.com>
2018-06-09 17:00:26 -0300

    .
    .

--- src/br/com/acme/Journal.java
+++ src/br/com/acme/Journal.java
@@ -1,14 +1,31 @@
 package br.com.acme;
 
 /**
- *
  * @authors Marcos Kuchak, Willian Patsche, William Hertz
  */
 public class Journal {
 
     private String name;
     private byte impactFactor;
+    private Publisher publisher;
 
+   public Journal(){
+        
+    }
+
+     /**
+     * Overloading do método construtor
+     * @param name do Journal
+     * @param impactFactor do Journal
+     * @param publisher do Journal
+     */
+   
+    public Journal(String name, byte impactFactor, Publisher publisher) {
+        this.name = name;
+        this.impactFactor = impactFactor;
+        this.publisher = publisher;
+    }
+     
     public String getName() {
         return name;
     }
